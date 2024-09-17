af7f405c640143ff9bb6e0d281a87e36c5a4463f
Daniel Huskey <daniel.huskey@icloud.com>
2024-09-16 23:18:35 -0400

    Initial commit of the Flutter calculator app

---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart'; // External package for expression evaluation
+//github copilot assised.
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _output = '';
+
+  void _onButtonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == '=') {
+        _output = _calculateResult();
+      } else if (buttonText == 'C') {
+        _output = '';
+      } else {
+        _output += buttonText;
+      }
+    });
+  }
+
+  String _calculateResult() {
+    try {
+        Expression expression = Expression.parse(_output);
+        //dont really need context for this simple calculator but it is required for the eval function so we pass in a val that will never be used
+        var context = {"x": 1};
+        
+        //following similar to the example in the package documentation
+        final evaluator = const ExpressionEvaluator();
+        var equals = evaluator.eval(expression, context);
+      return equals.toString();
+      
+    } catch (e) {
+      return 'Error' + e.toString();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _output,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('7'),
+              _buildButton('8'),
+              _buildButton('9'),
+              _buildButton('/'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('4'),
+              _buildButton('5'),
+              _buildButton('6'),
+              _buildButton('*'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('1'),
+              _buildButton('2'),
+              _buildButton('3'),
+              _buildButton('-'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('0'),
+              _buildButton('.'),
+              _buildButton('='),
+              _buildButton('+'),
+            ],
+          ),
+          SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildButton('C'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String buttonText) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () => _onButtonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
+    );
+  }
+}
